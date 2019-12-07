9f18b351a735f340b4ef431e4b16b484c3108f13
wong4835 <57549949+wong4835@users.noreply.github.com>
2019-12-07 12:59:31 +0900

    Update 003.py

--- 003.py
+++ 003.py
@@ -1,2 +1,7 @@
 #-*- coding:utf-8 -*-
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
 print('신씨가 소리질렀다. "도둑이야".')
