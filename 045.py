294d959ab8fd98237f5f44e496d8c49234bc4855
wong4835 <57549949+wong4835@users.noreply.github.com>
2019-12-28 18:41:53 +0900

    2019 12 28

--- 045.py
+++ 045.py
@@ -3,7 +3,7 @@ import sys
 import io
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
-
-movie_rank = ['닥터 스트레인지', '스플릿', '럭키', '배트맨']
-movie_rank.insert(1, "슈퍼맨")
+movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭키', '배트맨']
+movie_rank.remove('스플릿')
+movie_rank.remove('배트맨')
 print(movie_rank)
---
+++ 046.py
@@ -0,0 +1,3 @@
+lang1 = ["C", "C++", "JAVA"]
+lang2 = ["Python", "Go", "C#"]
+print(lang1 + lang2)
